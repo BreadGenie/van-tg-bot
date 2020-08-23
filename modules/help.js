5d141d382664aae1ef86025cb2137527c4564dc0
Bread Genie <msuhailbh07@gmail.com>
2020-08-23 17:15:36 +0530

    🤦 Add parse mode to help mwessage

--- modules/help.js
+++ modules/help.js
@@ -1,3 +1,5 @@
 exports.help = (bot, msg) => {
-  bot.sendMessage(msg.chat.id, "Hey, I am Van.\nA K-Pop DB Telegram bot!\n\nUse /group &lt;group-name&gt; to query a Group\n\nUse /idol &lt;idol-name&gt to query an Idol")
+  bot.sendMessage(msg.chat.id, "Hey, I am Van.\nA K-Pop DB Telegram bot!\n\nUse /group &lt;group-name&gt; to query a Group\n\nUse /idol &lt;idol-name&gt to query an Idol", {
+    parse_mode: "HTML"
+  })
 }

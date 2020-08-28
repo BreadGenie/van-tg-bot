20a0711182c03c04e6edb5fcbf6af5d420708125
Bread Genie <msuhailbh07@gmail.com>
2020-08-28 19:28:04 +0530

    🔄 Change random module method

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const help = require(__dirname + '/modules/help.js');
 const scrape = require(__dirname + '/modules/webscrape.js');
 const random = require(__dirname + '/modules/random.js');
 const token = process.env.TELEGRAM_TOKEN;
 const url = process.env.APP_URL;
+const mongodbURL = process.env.MONGO_URL;
 let options = {};
+var flag = false;
 
 if (process.env.PORT === undefined) {
   options = {
@@ -23,16 +26,58 @@ const bot = new TelegramBot(token, options);
 
 bot.setWebHook(`${url}/bot${token}`);
 
+mongoose.connect(mongodbURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const randomPicSchema = new mongoose.Schema({
+  fileID: [String],
+  fileUniqueID: [String],
+  group: String
+});
+
+const RandomPic = new mongoose.model("RandomPic", randomPicSchema);
+
+bot.on('message', (msg) => {
+  if (msg.chat.id === Number(process.env.RANDPIC_CHAT_ID) && flag === true && msg.photo) {
+    random.groupSetPic(bot, msg, RandomPic);
+  }
+});
+
+bot.onText(/^\/flag$/, msg => {
+  if (flag !== true) {
+    flag = true;
+    bot.sendMessage(msg.chat.id, "Flag is turned on!", {
+      reply_to_message_id: msg.message_id
+    });
+  } else {
+    flag = false;
+    bot.sendMessage(msg.chat.id, "Flag is turned off!", {
+      reply_to_message_id: msg.message_id
+    });
+  }
+});
+
+bot.onText(/^\/random($| )/, (msg, command) => {
+  random.randPic(bot, msg, command, RandomPic);
+});
+
+bot.onText(/^\/setrandpic($| )/, (msg, command) => {
+  random.setRandPic(bot, msg, command, RandomPic);
+});
+
+bot.onText(/^\/rmrandpic($|@VanBT21_Bot)/, (msg, command) => {
+  random.rmRandPic(bot, msg, command, RandomPic);
+});
+
 bot.onText(/^\/group($| )/, (msg, command) => {
   scrape.group(bot, msg, command);
 });
 
 bot.onText(/^\/idol($| )/, (msg, command) => {
   scrape.idol(bot, msg, command);
-});
-
-bot.onText(/^\/random($| )/, (msg, command) => {
-  random.random(bot, msg, command);
 });
 
 bot.onText(/^\/help($|@VanBT21_Bot)/, msg => {

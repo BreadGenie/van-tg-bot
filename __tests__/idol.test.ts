fd61e3d176825d6bba6e17cb23ee580383b26125
Bread Genie <msuhailbh07@gmail.com>
2021-07-28 14:20:28 +0530

    fix(tests): Iclude MBTI stats

--- __tests__/idol.test.ts
+++ __tests__/idol.test.ts
@@ -12,7 +12,7 @@ describe('/idol tests', () => {
   test('should check if idol details are returned properly (group)', async () => {
     const [idolPicLink, idolDescription] = await sendIdol('jin bts');
     const details =
-      '<u>Idol</u>\n\n<i>Jin (Kim SeokJin / 김석진)</i>\n\n<b>Group:</b> BTS\n<b>Label:</b> BIGHIT MUSIC\n<b>Nationality:</b> Korean\n<b>Birthdate:</b> Dec 04 1992\n<b>Blood Type:</b> O\n<b>Height:</b> 179 cm\n<b>Weight:</b> 63 kg\n<b>MBTI:</b> INTP\n<b>Position:</b> Vocal';
+      '<u>Idol</u>\n\n<i>Jin (Kim SeokJin / 김석진)</i>\n\n<b>Group:</b> BTS\n<b>Label:</b> BIGHIT MUSIC\n<b>Nationality:</b> Korean\n<b>Birthdate:</b> Dec 04 1992\n<b>Blood Type:</b> O\n<b>Height:</b> 179 cm\n<b>Weight:</b> 63 kg\n<b>MBTI:</b> INTP (Logician)\n<b>Position:</b> Vocal';
 
     expect(idolDescription).toBe(details);
     expect(idolPicLink).toMatch(/https:\/\/image.kpopmap.com\/(.*).(jpg|png)/);
@@ -21,7 +21,7 @@ describe('/idol tests', () => {
   test('should check if idol details are returned properly (solo)', async () => {
     const [idolPicLink, idolDescription] = await sendIdol('IU');
     const details =
-      '<u>Idol</u>\n\n<i>IU (Lee JiEun / 이지은)</i>\n\n<b>Label:</b> LOEN\n<b>Nationality:</b> Korean\n<b>Birthdate:</b> May 16 1993\n<b>Blood Type:</b> A\n<b>Height:</b> 162 cm\n<b>Weight:</b> 47 kg\n<b>Position:</b> Vocal';
+      '<u>Idol</u>\n\n<i>IU (Lee JiEun / 이지은)</i>\n\n<b>Label:</b> LOEN\n<b>Nationality:</b> Korean\n<b>Birthdate:</b> May 16 1993\n<b>Blood Type:</b> A\n<b>Height:</b> 162 cm\n<b>Weight:</b> 47 kg\n<b>MBTI:</b> INFP (Mediator)\n<b>Position:</b> Vocal';
 
     expect(idolDescription).toBe(details);
     expect(idolPicLink).toMatch(/https:\/\/image.kpopmap.com\/(.*).(jpg|png)/);

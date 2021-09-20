d27a74dcbcdd41eb9e50da72ee24b284d20c5742
Bread Genie <msuhailbh07@gmail.com>
2021-09-20 12:41:43 +0530

    refactor: idol Module

--- src/modules/idol.ts
+++ src/modules/idol.ts
@@ -2,59 +2,48 @@ import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import { readFileSync } from 'fs';
 import { SEND_ID, NO_ID } from '../helpers/strings';
-import { Idol, result } from '../types';
+import { Idol, ScrapedIdol } from '../types';
 import { matchStringArray } from '../helpers/dice';
 
-const scrapeIdol = async (foundIdol: Idol) => {
-  const result = await fetch(foundIdol.idolLink);
-  const body = await result.text();
+export const scrapeIdol = async (
+  foundIdols: Idol[]
+): Promise<ScrapedIdol[]> => {
+  const scrapedIdols: ScrapedIdol[] = [];
 
-  const $ = cheerio.load(body);
+  for (let i = 0; i < foundIdols.length; i++) {
+    const result = await fetch(foundIdols[i].idolLink);
+    const body = await result.text();
 
-  const foundCSS = $('#content-wrap > style').html();
-  const idolPicLink = foundCSS.match(/https(.*?)(jpg|png)/g);
-  let idolDescription =
-    '<u>Idol</u>\n\n<i>' + $('.profile-top > h2').text() + '</i>\n\n';
+    const $ = cheerio.load(body);
 
-  if ($('.profile-top .group').text() !== '') {
-    idolDescription +=
-      '<b>Group:</b> ' + $('.profile-top span').first().text() + '\n';
+    const foundCSS = $('#content-wrap > style').html();
+    const picLink = foundCSS.match(/https(.*?)(jpg|png)/g)[0];
+
+    const scrapedIdol: ScrapedIdol = {
+      picLink,
+      name: $('.profile-top > h2').text(),
+    };
+
+    if ($('.profile-top .group').text() !== '')
+      scrapedIdol.group = $('.profile-top span').first().text();
+
+    $('.half p').each((i, el) => {
+      scrapedIdol[$(el).prev().text().toLowerCase()] = $(el).text();
+    });
+
+    $('.full p').each((i, el) => {
+      scrapedIdol[$(el).prev().text().toLowerCase()] = $(el).text();
+    });
+
+    scrapedIdols.push(scrapedIdol);
   }
 
-  const idolDesc = [];
-
-  $('.half p').each((i, el) => {
-    idolDesc.push($(el).prev().text());
-    idolDesc.push($(el).text());
-  });
-
-  for (let i = 0; i < idolDesc.length; i += 2) {
-    idolDescription += '<b>' + idolDesc[i] + ':</b> ' + idolDesc[i + 1] + '\n';
-  }
-
-  $('.full p').each((i, el) => {
-    idolDesc.push($(el).prev().text());
-    idolDesc.push($(el).text());
-  });
-
-  if (idolDesc[idolDesc.length - 2] === 'SNS') {
-    idolDescription +=
-      '<b>' +
-      idolDesc[idolDesc.length - 4] +
-      ':</b> ' +
-      idolDesc[idolDesc.length - 3];
-  } else {
-    idolDescription +=
-      '<b>' +
-      idolDesc[idolDesc.length - 2] +
-      ':</b> ' +
-      idolDesc[idolDesc.length - 1];
-  }
-
-  return [idolPicLink[0], idolDescription];
+  return scrapedIdols;
 };
 
-export const searchIdol = async (findIdol: string): Promise<result> => {
+export const searchIdol = async (
+  findIdol: string
+): Promise<string | ScrapedIdol> => {
   if (findIdol === '') {
     return SEND_ID;
   } else {
@@ -69,7 +58,8 @@ export const searchIdol = async (findIdol: string): Promise<result> => {
 
     if (bestMatch[0].diceCoeff > 0.4) {
       const foundIdol = idols[bestMatch[0].index];
-      return await scrapeIdol(foundIdol);
+      const idolResult = await scrapeIdol([foundIdol]);
+      return idolResult[0];
     } else {
       return NO_ID;
     }

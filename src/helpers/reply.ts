c26d14b25ab01b9c154880fa36075d5097d5d2bf
Bread Genie <msuhailbh07@gmail.com>
2021-09-20 12:42:08 +0530

    refactor: reply helper

--- src/helpers/reply.ts
+++ src/helpers/reply.ts
@@ -1,10 +1,44 @@
 import TelegramBot = require('node-telegram-bot-api');
-import { result } from '../types';
+import { ScrapedGroup, ScrapedIdol } from '../types';
+
+export const prettifyReply = (result: ScrapedIdol | ScrapedGroup): string => {
+  let description = ``;
+  if (!('members' in result)) {
+    const idolKeys = Object.keys(result);
+
+    for (let i = 1; i < idolKeys.length; i++) {
+      if (i === 1)
+        description += `<u>Idol</u>\n\n<i>${result[idolKeys[i]]}</i>\n\n`;
+      else
+        description += `<b>${idolKeys[i].charAt(0).toUpperCase()}${idolKeys[
+          i
+        ].slice(1)}:</b> ${result[idolKeys[i]]}\n`;
+    }
+  } else {
+    const groupKeys = Object.keys(result);
+
+    for (let i = 1; i < groupKeys.length; i++) {
+      if (i !== groupKeys.length - 1)
+        description += `<b>${groupKeys[i].charAt(0).toUpperCase()}${groupKeys[
+          i
+        ].slice(1)}:</b> ${result[groupKeys[i]]}\n`;
+      else {
+        description += `<b>${groupKeys[i].charAt(0).toUpperCase()}${groupKeys[
+          i
+        ].slice(1)}:</b>\n`;
+        result[groupKeys[i]].forEach((member: string) => {
+          description += `<code>${member}</code>\n`;
+        });
+      }
+    }
+  }
+  return description;
+};
 
 export const sendReply = async (
   bot: TelegramBot,
   msg: TelegramBot.Message,
-  result: result
+  result: string | ScrapedIdol | ScrapedGroup
 ): Promise<void> => {
   if (typeof result === 'string') {
     bot.sendMessage(msg.chat.id, result, {
@@ -12,10 +46,12 @@ export const sendReply = async (
       parse_mode: 'HTML',
     });
   } else {
-    const [idolPicLink, groupDescription] = result;
+    const { picLink } = result;
 
-    bot.sendPhoto(msg.chat.id, idolPicLink, {
-      caption: groupDescription,
+    const description = prettifyReply(result);
+
+    bot.sendPhoto(msg.chat.id, picLink, {
+      caption: description,
       reply_to_message_id: msg.message_id,
       parse_mode: 'HTML',
     });

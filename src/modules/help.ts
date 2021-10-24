98d9caa526457559892e355f93d72b6047960073
Bread Genie <msuhailbh07@gmail.com>
2021-10-24 18:18:12 +0530

    feat: Add GitHub url in help message and format help message

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -1,11 +1,21 @@
 import TelegramBot = require('node-telegram-bot-api');
 
+const githubButton = {
+  inline_keyboard: [
+    [{
+      text: '🤖 Source Code',
+      url: 'https://github.com/BreadGenie/van-tg-bot'
+    }]
+  ]
+};
+
 const help = (bot: TelegramBot, msg: TelegramBot.Message): void => {
   bot.sendMessage(
     msg.chat.id,
-    'Hey, I am Van.\nA K-Pop DB Telegram bot!\n\nUse /group &lt;group-name&gt; to query a Group\n\nUse /idol &lt;idol-name&gt to query an Idol\nIf the idol name is 2 words then use /idol "&lt;idol-name&gt"',
+    'Hey, I am Van 🤖\nA K-Pop Telegram bot!\n\nUse /group &lt;group-name&gt; to query a Group\n\nUse /idol &lt;idol-name&gt to query an Idol',
     {
       parse_mode: 'HTML',
+      reply_markup: githubButton,
     }
   );
 };
